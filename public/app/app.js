ae351acc722a0326e69638c421e88cdc984c8ea5
lm <mapet241@poczta.fm>
2016-06-03 00:30:29 +0200

    [TEMP] Front page graphics changed

--- public/app/app.js
+++ public/app/app.js
@@ -1,25 +1,31 @@
 'use strict';
 
-angular.module('phonecatApp').
-    config(['$locationProvider', '$routeProvider',
-        function config($routeProvider) {
-            $routeProvider.
-                when('/phones', {
-                    template: '<phone-list></phone-list>'
-                }).
-                when('/phones/:phoneId', {
-                    template: '<phone-detail></phone-detail>'
-                }).
-                otherwise('/phones');
-        }
-    ]);
+var mainApp = angular.module('phonecatApp', []);
 
-angular.module('phonecatApp', []).controller('MainController', function ($scope) {
-    $scope.controllerOwner = {
-        imie: 'Lukasz',
-        nazwisko: 'Magierski'
-    };
-
+mainApp.controller('MainController', function ($scope) {
+    $scope.controllerOwner = 'Lukasz';
 });
+
+//     .
+//     config(['$locationProvider', '$routeProvider',
+//         function config($routeProvider) {
+//             $routeProvider.
+//                 when('/phones', {
+//                     template: '<phone-list></phone-list>'
+//                 }).
+//                 when('/phones/:phoneId', {
+//                     template: '<phone-detail></phone-detail>'
+//                 }).
+//                 otherwise('/phones');
+//         }
+//     ]);
+//
+// angular.module('phonecatApp', []).controller('MainController', function ($scope) {
+//     $scope.controllerOwner = {
+//         imie: 'Lukasz',
+//         nazwisko: 'Magierski'
+//     };
+//
+// });
 
 
